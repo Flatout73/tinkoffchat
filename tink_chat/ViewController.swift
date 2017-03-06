d0fbb2bd546ac9d91ea99a571de170c312aa2421
Flatout73 <fantom2259@gmail.com>
2017-03-06 03:01:58 +0300

    Initial Commit

---
+++ tink_chat/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  tink_chat
+//
+//  Created by Леонид Лядвейкин on 06.03.17.
+//  Copyright © 2017 Tinkoff Bank. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
